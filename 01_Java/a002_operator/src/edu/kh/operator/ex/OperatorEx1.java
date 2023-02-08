7ff3221664a9b5865b41ad22e54d8821801ff665
nalsong <pso7364@naver.com>
2023-02-08 11:58:45 +0900

    연산자1

--- 01_Java/a002_operator/src/edu/kh/operator/ex/OperatorEx1.java
+++ 01_Java/a002_operator/src/edu/kh/operator/ex/OperatorEx1.java
@@ -7,7 +7,7 @@ public class OperatorEx1 {
 	
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);  
+		Scanner sc = new Scanner(System.in); // Scanner 객체 생성
 		
 		
 		
