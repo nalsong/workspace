8daec66bfd44a68e72a55b6ab7bee5e3447507d8
nalsong <pso7364@naver.com>
2023-02-16 00:17:30 +0900

    20230216 배열 실습문제 수정1

--- 01_Java/a004_array/src/edu/kh/array/practice/ArrayPracticeRun.java
+++ 01_Java/a004_array/src/edu/kh/array/practice/ArrayPracticeRun.java
@@ -10,6 +10,27 @@ public class ArrayPracticeRun {
 //		ae1.practice2();
 //		ae1.practice3();
 		ae1.practice4();
+//		ae1.practice5();
+//		ae1.practice6();
+//		ae1.practice7();
+//		ae1.practice8();
+//		ae1.practice9();
+//		ae1.practice10();
+//		ae1.practice11();
+//		ae1.practice12();
+//		ae1.practice13();
+//		ae1.practice14();
+//		ae1.practice15();
+//		ae1.practice16();
+//		ae1.practice17();
+//		ae1.practice18();
+//		ae1.practice19();
+//		ae1.practice20();
+//		ae1.practice21();
+//		ae1.practice22();
+//		ae1.practice23();
+//		ae1.practice24();
+		
 		
 	}
 	
