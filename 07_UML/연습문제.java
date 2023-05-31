8c65850e4ae7675a5c4c2e74d1d41a553a94f330
정송이 <pso7364@naver.com>
2023-05-31 16:49:08 +0900

    20230531_UML

---
+++ 07_UML/연습문제.java
@@ -0,0 +1,195 @@
+
+1. 
+public class A{
+	private B b;
+}
+
+
+public class B{
+	private String f1;
+	private int f2;
+}
+
+
+2. 
+public class Test1{
+	private Test2 t2;
+	
+	public Test3 method1(){
+		Test3 t3 = new Reset3();
+		return t3;
+	}
+}
+
+
+public class Test2{
+		private int f1;
+}
+
+
+
+public class Test3{
+	private String str;
+
+	public String getStr(){
+		return str;
+	}
+
+	public void setStr(String str){
+		this.str = str;
+	}
+}
+
+
+3. 
+public abstract class Parent{
+	public void parentMethod(){
+		
+	}
+	protected abstract void print();
+}
+
+
+public class Child extends Parent{
+	private String name;
+
+	public void childMethod(){
+		return name;
+	}
+	protected void print(){
+		return name;
+	}
+}
+
+
+4. 
+
+public class Shape{
+	public void draw(){}
+	public void erase(){}
+	public int getLength(){}
+	protected abstract double getArea();
+}
+
+public interface Resizable{
+	void resize();
+}
+
+
+public class Triangle extends Shape implements Resizable{
+	public boolean isEquilateral(){}	
+	protected double getArea(){}
+	public void resize(){}
+
+}
+
+
+public class Rectangle extends Shape implements Resizable{
+	public boolean isSquare(){}
+	protected double getArea(){}
+	pulbic void resize(){}
+}
+
+
+5. 
+
+public class Student{
+	private String name;
+	private int age;
+}
+
+public class ClassRoom{
+	private Student student;
+
+	public ClassRoom(Student student){
+		this.student = student;
+	}
+}
+
+
+
+
+6.
+public class Book{
+	private String title;
+	private String author;
+}
+
+public class Library{
+	private Book book;
+
+	public Library(){
+		this.book = new Book();
+	}
+}
+
+
+
+7.
+
+public class Employee{
+	private String name;
+}
+
+public class Product{
+	private String pName;
+	private int price;
+}
+
+public class Shop{
+	private Product product;
+	private Employee emp;
+
+	public Shop(Product p){
+		this.p = p;
+		this.emp = new Employee();
+	}
+}
+
+
+
+8. 
+public class abstract Coffee{
+	public String beans;
+}
+
+
+public class Option{
+	private int shot;
+	private String ice;
+	private int syrup;
+}
+
+
+public class extends CafeLatte{
+	private String temperature;
+	private int price;
+
+	public void drink{
+		Option option = new Option();
+		option.shot;
+		option.ice;
+		option.syrup;
+	}
+}
+
+
+bublic class Cafe{
+	private String name;
+	private String address;
+	private CafeLatte latte;
+
+	public Cafe(CafeLatte latte){
+		this.latte = latte;
+	}
+
+}
+
+
+
+
+
+
+
+
+
