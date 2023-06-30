e0011b93f78ad948318ff5e0334b6e094e6abcdc
정송이 <pso7364@naver.com>
2023-06-30 10:30:48 +0900

    react- props

---
+++ 08_React/my-app/src/components/R01_props.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+// props : 부모 컴포넌트가 자식 컴포넌트에게 데이터 전달 시 사용하는 객체
+// *** props는 자식 -> 부모 데이터 전달은 불가능 ***
+
+//====================
+test(); // 함수(function)를 먼저 해석하기 때문에 함수보다 앞에 변수 선언 가능
+
+// function 으로 시작하는 함수
+function test(){
+    console.log('test');
+}
+// ====================
+
+
+const ChildComponent = (props) =>{
+    return(
+        <>
+            <ul>
+                <li>이름: {props.name}</li>
+                <li>나이: {props.age}</li>
+            </ul>
+        </>
+    );
+
+}
+
+const MenuPrint =(props) => {
+    return(
+        <h4>김밥 : {props.김밥}, 떡볶이 :{props.떡볶이}</h4>
+    );
+}
+
+// 하단의 함수는 변수선언 불가
+const PropsEx = (props) => {
+                        // props 매개변수 : 부모로 부터 전달 받은 값이 담겨 있는 객체
+    console.log(props);
+    console.log(props.name);
+
+    const menu = {'김밥' : 3000, '떡볶이' : 4000};
+
+    return(
+        <>
+        <h1>{props.name}</h1>
+        <ChildComponent name={props.name} age={props.name === '홍길동' ? 20 : 25} />
+        <MenuPrint {...menu}/>
+        {/* <MenuPrint {김밥 ={3000}, 떡볶이 ={ 4000}/> */}
+        </>
+    );
+}
+
+export default PropsEx;
